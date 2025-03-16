d446ce9cf089c0b32ccf7480d1fe0fea5a526b5e
1ZV1 <rosenwagen@tutamail.com>
2025-03-17 01:12:18 +0300

    SumAllPrimeInArrayHEADER

--- function.h
+++ function.h
@@ -6,5 +6,4 @@ void InputArr(T* arr, int32_t ArraySize);
 template <typename T>
 int32_t FindElement(T* arr,T elem, int32_t size);
 
-template<>
-int32_t FindElement<char>(char* arr,char elem, int32_t size);
+int64_t SumAllPrimeInArray(int32_t *arr, int32_t ArraySize);
